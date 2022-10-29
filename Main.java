a4c3ecf42a0f1a1b4c004c4ea162ad2c41270605
John Laurence Castillo <21103853@usc.edu.ph>
2022-10-29 08:26:07 +0800

    saveupdate

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String[] args){
-System.out.println("My Favorite Artists");
-System.out.println("Jin ");
-System.out.println("Cha eun woo");
-System.out.println("jang uk");
+System.out.println("Vegetables");
+System.out.println("ampalaya ");
+System.out.println("Carrots");
+System.out.println("Malunggay");
     }
     }
