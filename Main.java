2719414cfcaad1b9bad3b9a8679000bf30408522
John Laurence Castillo <21103853@usc.edu.ph>
2022-10-29 08:16:34 +0800

    savesecondfiletomerge

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args){
-System.out.println("My favorite musics");
+System.out.println("Only Hope");
+System.out.println("Life goes on");
+System.out.println("DNA");
+System.out.println("Wakas");
     }
     }
