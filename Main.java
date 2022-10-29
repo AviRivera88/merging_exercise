b0794dc05bad6bd55b15f32af33aa2d2e517f27f
John Laurence Castillo <21103853@usc.edu.ph>
2022-10-29 08:39:42 +0800

    saveanotherfileinthirdbranch

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 public class Main{
     public static void main(String[] args){
 
-System.out.println("My Favorite Artists");
-System.out.println("Jin ");
-System.out.println("Cha eun woo");
-System.out.println("jang uk");
-System.out.println("lee jung suk");
+System.out.println("Hello I am Via Borrer Enopia");
+System.out.println("And I am very thankful for your support and love");
+System.out.println("I hope you find this video helpful and interesting..");
 
-System.out.println("Vegetables");
-System.out.println("ampalaya ");
-System.out.println("Carrots");
-System.out.println("Malunggay");
     }
     }
